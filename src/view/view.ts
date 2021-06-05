aabf812c29aa6bdf15176bd24d2f023507d2e0d4
diyanious <dcairns@wgu.edu>
2021-06-05 13:33:09 +0800

    ui update

---
+++ src/view/view.ts
@@ -0,0 +1,18 @@
+class View {
+  constructor() {
+    this.app = this.getElement('#root');
+  }
+
+  getElement(selector) {
+    const element = document.querySelector(selector);
+
+    return element;
+  }
+
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+
+    return element;
+  }
+}
