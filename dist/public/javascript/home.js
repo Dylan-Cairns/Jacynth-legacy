8ef08e6d574cb93bc614bf84f5cfd0cddcb6fb12
diyanious <dcairns@wgu.edu>
2021-07-29 19:38:58 +0800

    fixed resume button not appearing on home page

--- dist/public/javascript/home.js
+++ dist/public/javascript/home.js
@@ -1,5 +1,12 @@
 "use strict";
 var _a;
+// disable bfcache to force the page to reload every time,
+// even when visited from back button. This is to
+// Ensure that the single player resume button displays
+// at the correct times.
+// relevant link: https://stackoverflow.com/questions/2638292/after-travelling-back-in-firefox-history-javascript-wont-run
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+window.onunload = function () { };
 const newGameButton = document.getElementById('newGameBttn');
 const menu = document.getElementById('menu-popup');
 const rulesButton = document.getElementById('rulesButton');
--- src/public/javascript/home.ts
+++ src/public/javascript/home.ts
@@ -1,3 +1,11 @@
+// disable bfcache to force the page to reload every time,
+// even when visited from back button. This is to
+// Ensure that the single player resume button displays
+// at the correct times.
+// relevant link: https://stackoverflow.com/questions/2638292/after-travelling-back-in-firefox-history-javascript-wont-run
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+window.onunload = function () {};
+
 const newGameButton = document.getElementById(
   'newGameBttn'
 ) as HTMLAnchorElement;
