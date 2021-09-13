21c0ab9e220da48fdad17f0e65a02061021a25c1
diyanious <dcairns@wgu.edu>
2021-09-13 13:04:32 -0600

    update auth config

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -1,25 +1,23 @@
 import express from 'express';
 export const authRouter = express();
-authRouter.get('/sign-up/:page', (req, res) => {
-    const { page } = req.params;
+authRouter.get('/sign-up/', (req, res) => {
     res.oidc.login({
-        returnTo: page,
         authorizationParams: {
             screen_hint: 'signup'
         }
     });
 });
-authRouter.get('/login/:page', (req, res) => {
-    const { page } = req.params;
-    console.log(page);
-    res.oidc.login({
-        returnTo: page
-    });
-});
-authRouter.get('/logout/:page', (req, res) => {
-    const { page } = req.params;
-    console.log(page);
-    res.oidc.logout({
-        returnTo: page
-    });
-});
+// authRouter.get('/login/:page', (req, res) => {
+//   const { page } = req.params;
+//   console.log(page);
+//   res.oidc.login({
+//     returnTo: page
+//   });
+// });
+// authRouter.get('/logout/:page', (req, res) => {
+//   const { page } = req.params;
+//   console.log(page);
+//   res.oidc.logout({
+//     returnTo: page
+//   });
+// });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,31 +2,28 @@ import express from 'express';
 
 export const authRouter = express();
 
-authRouter.get('/sign-up/:page', (req, res) => {
-  const { page } = req.params;
-
+authRouter.get('/sign-up/', (req, res) => {
   res.oidc.login({
-    returnTo: page,
     authorizationParams: {
       screen_hint: 'signup'
     }
   });
 });
 
-authRouter.get('/login/:page', (req, res) => {
-  const { page } = req.params;
-  console.log(page);
+// authRouter.get('/login/:page', (req, res) => {
+//   const { page } = req.params;
+//   console.log(page);
 
-  res.oidc.login({
-    returnTo: page
-  });
-});
+//   res.oidc.login({
+//     returnTo: page
+//   });
+// });
 
-authRouter.get('/logout/:page', (req, res) => {
-  const { page } = req.params;
-  console.log(page);
+// authRouter.get('/logout/:page', (req, res) => {
+//   const { page } = req.params;
+//   console.log(page);
 
-  res.oidc.logout({
-    returnTo: page
-  });
-});
+//   res.oidc.logout({
+//     returnTo: page
+//   });
+// });
